d6b68cc7c3539cf3937a8eac0fb5699c3fee93c7
cameron-sutterfield <94657726+cameron-sutterfield@users.noreply.github.com>
2022-04-06 16:36:55 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace BurgerStand
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbKids = new System.Windows.Forms.RadioButton();
+            this.rbSmall = new System.Windows.Forms.RadioButton();
+            this.rbMedium = new System.Windows.Forms.RadioButton();
+            this.rbLarge = new System.Windows.Forms.RadioButton();
+            this.rbFries = new System.Windows.Forms.RadioButton();
+            this.rbCurlyFries = new System.Windows.Forms.RadioButton();
+            this.rbTaterTots = new System.Windows.Forms.RadioButton();
+            this.cbCheese = new System.Windows.Forms.CheckBox();
+            this.cbKetchup = new System.Windows.Forms.CheckBox();
+            this.cbMustard = new System.Windows.Forms.CheckBox();
+            this.cbLettuce = new System.Windows.Forms.CheckBox();
+            this.cbTomato = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(118, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CAMERONS BURGER SHACK";
+            // 
+            // rbKids
+            // 
+            this.rbKids.AutoSize = true;
+            this.rbKids.Location = new System.Drawing.Point(77, 136);
+            this.rbKids.Name = "rbKids";
+            this.rbKids.Size = new System.Drawing.Size(49, 17);
+            this.rbKids.TabIndex = 1;
+            this.rbKids.TabStop = true;
+            this.rbKids.Text = "Kids";
+            this.rbKids.UseVisualStyleBackColor = true;
+            // 
+            // rbSmall
+            // 
+            this.rbSmall.AutoSize = true;
+            this.rbSmall.Location = new System.Drawing.Point(77, 186);
+            this.rbSmall.Name = "rbSmall";
+            this.rbSmall.Size = new System.Drawing.Size(55, 17);
+            this.rbSmall.TabIndex = 2;
+            this.rbSmall.TabStop = true;
+            this.rbSmall.Text = "Small";
+            this.rbSmall.UseVisualStyleBackColor = true;
+            // 
+            // rbMedium
+            // 
+            this.rbMedium.AutoSize = true;
+            this.rbMedium.Location = new System.Drawing.Point(77, 240);
+            this.rbMedium.Name = "rbMedium";
+            this.rbMedium.Size = new System.Drawing.Size(68, 17);
+            this.rbMedium.TabIndex = 3;
+            this.rbMedium.TabStop = true;
+            this.rbMedium.Text = "Medium";
+            this.rbMedium.UseVisualStyleBackColor = true;
+            // 
+            // rbLarge
+            // 
+            this.rbLarge.AutoSize = true;
+            this.rbLarge.Location = new System.Drawing.Point(77, 297);
+            this.rbLarge.Name = "rbLarge";
+            this.rbLarge.Size = new System.Drawing.Size(57, 17);
+            this.rbLarge.TabIndex = 4;
+            this.rbLarge.TabStop = true;
+            this.rbLarge.Text = "Large";
+            this.rbLarge.UseVisualStyleBackColor = true;
+            // 
+            // rbFries
+            // 
+            this.rbFries.AutoSize = true;
+            this.rbFries.Location = new System.Drawing.Point(226, 136);
+            this.rbFries.Name = "rbFries";
+            this.rbFries.Size = new System.Drawing.Size(52, 17);
+            this.rbFries.TabIndex = 5;
+            this.rbFries.TabStop = true;
+            this.rbFries.Text = "Fries";
+            this.rbFries.UseVisualStyleBackColor = true;
+            // 
+            // rbCurlyFries
+            // 
+            this.rbCurlyFries.AutoSize = true;
+            this.rbCurlyFries.Location = new System.Drawing.Point(226, 186);
+            this.rbCurlyFries.Name = "rbCurlyFries";
+            this.rbCurlyFries.Size = new System.Drawing.Size(84, 17);
+            this.rbCurlyFries.TabIndex = 6;
+            this.rbCurlyFries.TabStop = true;
+            this.rbCurlyFries.Text = "Curly Fries";
+            this.rbCurlyFries.UseVisualStyleBackColor = true;
+            // 
+            // rbTaterTots
+            // 
+            this.rbTaterTots.AutoSize = true;
+            this.rbTaterTots.Location = new System.Drawing.Point(226, 240);
+            this.rbTaterTots.Name = "rbTaterTots";
+            this.rbTaterTots.Size = new System.Drawing.Size(84, 17);
+            this.rbTaterTots.TabIndex = 7;
+            this.rbTaterTots.TabStop = true;
+            this.rbTaterTots.Text = "Tater Tots";
+            this.rbTaterTots.UseVisualStyleBackColor = true;
+            // 
+            // cbCheese
+            // 
+            this.cbCheese.AutoSize = true;
+            this.cbCheese.Location = new System.Drawing.Point(379, 137);
+            this.cbCheese.Name = "cbCheese";
+            this.cbCheese.Size = new System.Drawing.Size(68, 17);
+            this.cbCheese.TabIndex = 8;
+            this.cbCheese.Text = "Cheese";
+            this.cbCheese.UseVisualStyleBackColor = true;
+            // 
+            // cbKetchup
+            // 
+            this.cbKetchup.AutoSize = true;
+            this.cbKetchup.Location = new System.Drawing.Point(379, 187);
+            this.cbKetchup.Name = "cbKetchup";
+            this.cbKetchup.Size = new System.Drawing.Size(73, 17);
+            this.cbKetchup.TabIndex = 9;
+            this.cbKetchup.Text = "Ketchup";
+            this.cbKetchup.UseVisualStyleBackColor = true;
+            // 
+            // cbMustard
+            // 
+            this.cbMustard.AutoSize = true;
+            this.cbMustard.Location = new System.Drawing.Point(379, 241);
+            this.cbMustard.Name = "cbMustard";
+            this.cbMustard.Size = new System.Drawing.Size(71, 17);
+            this.cbMustard.TabIndex = 10;
+            this.cbMustard.Text = "Mustard";
+            this.cbMustard.UseVisualStyleBackColor = true;
+            // 
+            // cbLettuce
+            // 
+            this.cbLettuce.AutoSize = true;
+            this.cbLettuce.Location = new System.Drawing.Point(379, 297);
+            this.cbLettuce.Name = "cbLettuce";
+            this.cbLettuce.Size = new System.Drawing.Size(69, 17);
+            this.cbLettuce.TabIndex = 11;
+            this.cbLettuce.Text = "Lettuce";
+            this.cbLettuce.UseVisualStyleBackColor = true;
+            // 
+            // cbTomato
+            // 
+            this.cbTomato.AutoSize = true;
+            this.cbTomato.Location = new System.Drawing.Point(379, 349);
+            this.cbTomato.Name = "cbTomato";
+            this.cbTomato.Size = new System.Drawing.Size(68, 17);
+            this.cbTomato.TabIndex = 12;
+            this.cbTomato.Text = "Tomato";
+            this.cbTomato.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 392);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "!!Best Burger Shack In Town!!!";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 423);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 24);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Total";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(460, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 471);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbTomato);
+            this.Controls.Add(this.cbLettuce);
+            this.Controls.Add(this.cbMustard);
+            this.Controls.Add(this.cbKetchup);
+            this.Controls.Add(this.cbCheese);
+            this.Controls.Add(this.rbTaterTots);
+            this.Controls.Add(this.rbCurlyFries);
+            this.Controls.Add(this.rbFries);
+            this.Controls.Add(this.rbLarge);
+            this.Controls.Add(this.rbMedium);
+            this.Controls.Add(this.rbSmall);
+            this.Controls.Add(this.rbKids);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbKids;
+        private System.Windows.Forms.RadioButton rbSmall;
+        private System.Windows.Forms.RadioButton rbMedium;
+        private System.Windows.Forms.RadioButton rbLarge;
+        private System.Windows.Forms.RadioButton rbFries;
+        private System.Windows.Forms.RadioButton rbCurlyFries;
+        private System.Windows.Forms.RadioButton rbTaterTots;
+        private System.Windows.Forms.CheckBox cbCheese;
+        private System.Windows.Forms.CheckBox cbKetchup;
+        private System.Windows.Forms.CheckBox cbMustard;
+        private System.Windows.Forms.CheckBox cbLettuce;
+        private System.Windows.Forms.CheckBox cbTomato;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BurgerStand
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Variables
+            const double Kids = 3.00;
+            const double Small = 4.00;
+            const double Medium = 5.00;
+            const double Large = 6.00;
+            const double Fries = 0;
+            const double CurlyFries = 1.00;
+            const double TattorTots = .50;
+            const double Cheese = .50;
+            const double Vegetables = .05;
+            const double Condiments = .10;
+            const double tax = .10;
+            double taxes;
+            double totalPrice;
+            double addOn = 0;
+
+            // Meal size
+            if (rbKids.Checked)
+            {
+                totalPrice = Kids;
+            }
+
+            else
+                if (rbSmall.Checked)
+            {
+                totalPrice = Small;
+            }
+
+            else
+                if (rbMedium.Checked)
+            {
+                totalPrice = Medium;
+            }
+
+            else
+                totalPrice = Large;
+
+            // Sides
+            if (rbFries.Checked)
+            {
+                totalPrice = totalPrice + Fries;
+            }
+
+            else
+                if (rbCurlyFries.Checked)
+            {
+                totalPrice = totalPrice + CurlyFries;
+            }
+
+            else
+                totalPrice = totalPrice + TattorTots;
+
+            //Add-on and condiments
+            if (cbCheese.Checked)
+            {
+                addOn = addOn + Cheese;
+            }
+
+            if (cbKetchup.Checked)
+            {
+                addOn = addOn + Condiments;
+            }
+
+            if (cbMustard.Checked)
+            {
+                addOn = addOn + Condiments;
+            }
+
+            if (cbLettuce.Checked)
+            {
+                addOn = addOn + Vegetables;
+            }
+
+            if (cbTomato.Checked)
+            {
+                addOn = addOn + Vegetables;
+            }
+
+            //Total 
+            totalPrice = totalPrice + addOn;
+
+            taxes = totalPrice * tax;
+
+            totalPrice = totalPrice + taxes;
+
+            //Output
+            label2.Text = "Final Price: " + totalPrice.ToString("C2");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
